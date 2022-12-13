75ccbdbe2ab8b05caffa9d91421bb1aba8c04b39
nh3000-org <118835575+nh3000-org@users.noreply.github.com>
2022-12-13 10:41:13 -0600

    Update messages.go

--- panes/messages.go
+++ panes/messages.go
@@ -1,23 +1,34 @@
 package panes
 
 import (
-
-
 	"log"
-
 
 	"fyne.io/fyne/v2"
 
-
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
-
 )
 
-func messagesScreen(_ fyne.Window) {
+var Encmessage string // message strung
+func messagesScreen(_ fyne.Window) fyne.CanvasObject {
 
 	log.Println("messagesScreen")
-	password := widget.NewEntry()
-	password.SetPlaceHolder("Enter Password For Encryption")
-	password.SetText(Password)
+	mymessage := widget.NewEntry()
+	mymessage.SetPlaceHolder("Enter Message For Encryption")
 
+	// try the password
+	smbutton := widget.NewButton("Send Message", func() {
+
+		Encmessage = mymessage.Text
+
+	})
+	return container.NewCenter(container.NewVBox(
+		widget.NewLabelWithStyle("New Horizons 3000 Secure Communications", fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
+
+		mymessage,
+
+		smbutton,
+
+		widget.NewLabel(""), // balance the header on the tutorial screen we leave blank on this content
+	))
 }
