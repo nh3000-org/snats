7ea8b55583162936ec5cb43e7a78bde4c99814f8
nh3000-org <118835575+nh3000-org@users.noreply.github.com>
2022-12-13 10:41:55 -0600

    Update data.go

--- panes/data.go
+++ panes/data.go
@@ -4,29 +4,22 @@ import (
 	"fyne.io/fyne/v2"
 )
 
-// Tutorial defines the data structure for a tutorial
-type Panes struct {
+// Pane defines the data structure
+type MyPane struct {
 	Title, Intro string
 	View         func(w fyne.Window) fyne.CanvasObject
 	SupportWeb   bool
 }
 
 var (
-	// Tutorials defines the metadata for each tutorial
-	Panes = map[string]Tutorial{
-		"logon": {"Logon", "", logonScreen, true},
-    "messages": {"Messages", "", messageScreen, true},
-		
-		"apptabs": {"AppTabs",
-			"A container to help divide up an application into functional areas.",
-			makeAppTabsTab,
-			true,
-		},
+	// Panes defines the metadata
+	MyPanes = map[string]MyPane{
+		"logon":    {"Logon", "", logonScreen, true},
+		"messages": {"Messages", "", messagesScreen, true},
 	}
 
-	// TutorialIndex  defines how our tutorials should be laid out in the index tree
-	TutorialIndex = map[string][]string{
-		"":            {"logon", "messages"},
-
+	// PanesIndex  defines how our panes should be laid out in the index tree
+	MyPanesIndex = map[string][]string{
+		"": {"logon", "messages"},
 	}
 )
