604031b16ad5dfb4f37850524823fe522c64992b
nh3000-org <118835575+nh3000-org@users.noreply.github.com>
2023-06-09 18:38:46 -0500

    BETA.2

--- panes/data.go
+++ panes/data.go
@@ -11,6 +11,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"unicode"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -33,9 +34,9 @@ var MyBytes = []byte{35, 46, 57, 24, 85, 35, 24, 74, 87, 35, 88, 98, 66, 32, 14,
 const MySecret string = "abd&1*~#^2^#s0^=)^^7%c34"                                   // must be 24 characters
 const MyDurable string = "snatsdurable"
 const PasswordDefault = "123456" // default password shipped with app
-var Caroot = string("-----BEGIN CERTIFICATE-----\nMIICFDCCAbugAwIBAgIUDkHxHO1DwrlkTzUimG5PoiswB6swCgYIKoZIzj0EAwIw\nZjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkZMMQswCQYDVQQHEwJDVjEMMAoGA1UE\nChMDU0VDMQwwCgYDVQQLEwNuaDExITAfBgNVBAMTGG5hdHMubmV3aG9yaXpvbnMz\nMDAwLm9yZzAgFw0yMzAzMzExNzI5MDBaGA8yMDUzMDMyMzE3MjkwMFowZjELMAkG\nA1UEBhMCVVMxCzAJBgNVBAgTAkZMMQswCQYDVQQHEwJDVjEMMAoGA1UEChMDU0VD\nMQwwCgYDVQQLEwNuaDExITAfBgNVBAMTGG5hdHMubmV3aG9yaXpvbnMzMDAwLm9y\nZzBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABHXwMUfMXiJix3tuzFymcA+3RkeY\nZE7urUzVgaqkv/Oef3jhqhtf1XzK/qVYGxWWmpvADGB252PG1Mp7Z5wmzqyjRTBD\nMA4GA1UdDwEB/wQEAwIBBjASBgNVHRMBAf8ECDAGAQH/AgEBMB0GA1UdDgQWBBQm\nFA5caanuqxGFOf9DtZkVYv5dCzAKBggqhkjOPQQDAgNHADBEAiB3BheNP4XdBZ27\nxVBQ7ztMJqK7wDi1V3LuMy5jmXr7rQIgHCse0oaiAwcl4VwF00aSshlV+T/da0Tx\n1ANkaM+rie4=\n-----END CERTIFICATE-----\n")
-var Clientcert = string("-----BEGIN CERTIFICATE-----\nMIIDUzCCAvigAwIBAgIUUyhlJt8mp1XApRbSkdrUS55LGV8wCgYIKoZIzj0EAwIw\nZjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkZMMQswCQYDVQQHEwJDVjEMMAoGA1UE\nChMDU0VDMQwwCgYDVQQLEwNuaDExITAfBgNVBAMTGG5hdHMubmV3aG9yaXpvbnMz\nMDAwLm9yZzAeFw0yMzAzMzExNzI5MDBaFw0yODAzMjkxNzI5MDBaMHIxCzAJBgNV\nBAYTAlVTMRAwDgYDVQQIEwdGbG9yaWRhMRIwEAYDVQQHEwlDcmVzdHZpZXcxGjAY\nBgNVBAoTEU5ldyBIb3Jpem9ucyAzMDAwMSEwHwYDVQQLExhuYXRzLm5ld2hvcml6\nb25zMzAwMC5vcmcwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDFttVH\nQ131JYwazAQMm0XAQvRvTjTjOY3aei1++mmQ+NQ9mrOFk6HlZFoKqsy6+HPXsB9x\nQbWlYvUOuqBgb9xFQZoL8jiKskLLrXoIxUAlIBTlyf76r4SV+ZpxJYoGzXNTedaU\n0EMTyAiUQ6nBbFMXiehN5q8VzxtTESk7QguGdAUYXYsCmYBvQtBXoFYO5CHyhPqu\nOZh7PxRAruYypEWVFBA+29+pwVeaRHzpfd/gKLY4j2paInFn7RidYUTqRH97BjdR\nSZpOJH6fD7bI4L09pnFtII5pAARSX1DntS0nWIWhYYI9use9Hi/B2DRQLcDSy1G4\n0t1z4cdyjXxbFENTAgMBAAGjgawwgakwDgYDVR0PAQH/BAQDAgWgMBMGA1UdJQQM\nMAoGCCsGAQUFBwMCMAwGA1UdEwEB/wQCMAAwHQYDVR0OBBYEFAzgPVB2/sfT7R0U\ne3iXRSvUkfoQMB8GA1UdIwQYMBaAFCYUDlxpqe6rEYU5/0O1mRVi/l0LMDQGA1Ud\nEQQtMCuCGG5hdHMubmV3aG9yaXpvbnMzMDAwLm9yZ4IJMTI3LDAsMCwxhwTAqABn\nMAoGCCqGSM49BAMCA0kAMEYCIQCDlUH2j69mJ4MeKvI8noOmvLHfvP4qMy5nFW2F\nPT5UxgIhAL6pHFyEbANtSkcVJqxTyKE4GTXcHc4DB43Z1F7VxSJj\n-----END CERTIFICATE-----\n")
-var Clientkey = string("-----BEGIN RSA PRIVATE KEY-----\nMIIEowIBAAKCAQEAxbbVR0Nd9SWMGswEDJtFwEL0b0404zmN2notfvppkPjUPZqz\nhZOh5WRaCqrMuvhz17AfcUG1pWL1DrqgYG/cRUGaC/I4irJCy616CMVAJSAU5cn+\n+q+ElfmacSWKBs1zU3nWlNBDE8gIlEOpwWxTF4noTeavFc8bUxEpO0ILhnQFGF2L\nApmAb0LQV6BWDuQh8oT6rjmYez8UQK7mMqRFlRQQPtvfqcFXmkR86X3f4Ci2OI9q\nWiJxZ+0YnWFE6kR/ewY3UUmaTiR+nw+2yOC9PaZxbSCOaQAEUl9Q57UtJ1iFoWGC\nPbrHvR4vwdg0UC3A0stRuNLdc+HHco18WxRDUwIDAQABAoIBACe0XMZP4Al//c/P\n0qxZbjt69q13jiVnhHYwfPx3+0UywySP8adMi4GOkop73Ftb05+n7diHspvA8KeB\nkP1s2VZLI01s2i/4NnPCpbQnMIeEFs5Cr2LWZpDbrEk2ma5eCd/kotQFssLBM//a\nSrfeMh2TA0TJo7WEft9Cnf4ZeEkKnycplfvwTyv286iFZCYo2dv66BfTej6kkVCo\nAi+ZVCe2zSqRYyr0u4/j/kE3b3eSkCnY2IVcqlP7epuEGVOZyxeFLwM5ljbWL816\npA6WIJgQo2EQ1N7L531neg5WjXQ/UwTQoXP1jvuuVtKtOBFqm1IshEyFk3WpsfpD\nr16OTdECgYEA6FB6NYxYtnWPaIYAOqP7GtMKoJujH8MtZy6J33LkxI7nPkMkn8Mv\nva32tvjU4Bu1FVNp9k5guC+b+8ixXK0URj25IOhDs6K57tck22W9WiTZlmnkCO01\nJOavrelWbvYt5xNWIdnPualoPfGB0iJKXsKY/bpH4eVfhWwpNPI5sMkCgYEA2d9G\nEPuWN6gUjZ+JfdS+0WHK1yGD7thXs7MPUlhGqDzBryh2dkywyo8U8+tMLuDok1RZ\njnT3PYkLQEpzoV0qBkpFFShL6ubaGmDz1UZsozl0YcIg4diZeuPHnIAeXOFrhgYf\n825163LmT3jYHCROFEMLtTYyIQP0EznE+qFT3TsCgYEApgtvbfqkJbWdDL5KR5+R\nCLky7VyQmVEtkIRI8zbxoDPrwCrJcI9X/iDrKBhuPshPA7EdGXkn1D3jJXFqo6zp\nwtK3EXgxe6Ghd766jz4Guvl/s+x3mpHA3GEtzAXtS14VrQW7GHLP8AnPggauHX14\n3oYER8XvPtxtC7YlNbyz01ECgYAe2b7SKM3ck7BVXYHaj4V1oKNYUyaba4b/qxtA\nTb+zkubaJqCfn7xo8lnFMExZVv+X3RnRUj6wN/ef4ur8rnSE739Yv5wAZy/7DD96\ns74uXrRcI2EEmechv59ESeACxuiy0as0jS+lZ1+1YSc41Os5c0T1I/d1NVoaXtPF\nqZJ2gQKBgBp/XavdULBPzC7B8tblySzmL01qJZV7MSSVo2/1vJ7gPM0nQPZdTDog\nTfA5QKSX9vFTGC9CZHSJ+fabYDDd6+3UNYUKINfr+kwu9C2cysbiPaM3H27WR5mW\n5LhStAfwuRRYBDsG2ndjraxcBrrPdtkbS0dpeQUDJxvkMIuLHnhQ\n-----END RSA PRIVATE KEY-----\n")
+var Caroot = ""
+var Clientcert = ""
+var Clientkey = ""
 
 var MyMap = make(map[string]int)
 var MyApp fyne.App
@@ -87,16 +88,17 @@ type MessageStore struct {
 
 var (
 	MyPanes = map[string]MyPane{
-		"password": {"Pass Reset", "", passwordScreen, true},
-		"settings": {"Settings", "", settingsScreen, true},
-		"logon":    {"Logon", "", logonScreen, true},
-		"messages": {"Messages", "", messagesScreen, true},
-		"encdec":   {"Enc/Dec", "", encdecScreen, true},
+		"password":     {"Pass Reset", "", passwordScreen, true},
+		"settings":     {"Settings", "", settingsScreen, true},
+		"certificates": {"Certificates", "", certificatesScreen, true},
+		"logon":        {"Logon", "", logonScreen, true},
+		"messages":     {"Messages", "", messagesScreen, true},
+		"encdec":       {"Enc/Dec", "", encdecScreen, true},
 	}
 
 	// PanesIndex  defines how our panes should be laid out in the index tree
 	MyPanesIndex = map[string][]string{
-		"": {"password", "logon", "settings", "messages", "encdec"},
+		"": {"password", "logon", "settings", "certificates", "messages", "encdec"},
 	}
 )
 
@@ -130,19 +132,33 @@ func MyJson(action string) {
 	}
 	MyApp = GetMyApp()
 	if action == "LOAD" {
-		xServer, _ := Encrypt(Server, MySecret)
+		// prepare fallback or just load
+		xServer, _ := Encrypt("nats://nats.newhorizons3000.org:4222", MySecret)
 		Server = MyApp.Preferences().StringWithFallback("Server", xServer)
-		xQueue, _ := Encrypt(Queue, MySecret)
+		xQueue, _ := Encrypt("MESSAGES", MySecret)
 		Queue = MyApp.Preferences().StringWithFallback("Queue", xQueue)
-		xAlias, _ := Encrypt(Alias, MySecret)
+		xAlias, _ := Encrypt("MyAlias", MySecret)
 		Alias = MyApp.Preferences().StringWithFallback("Alias", xAlias)
-		xQueuepassword, _ := Encrypt(Queuepassword, MySecret)
-		Queuepassword = MyApp.Preferences().StringWithFallback("Queuepassword", xQueuepassword)
-		xPasswordMinimumSize, _ := Encrypt("6", MySecret)
-		PasswordMinimumSize = MyApp.Preferences().StringWithFallback("PasswordMinimumSize", xPasswordMinimumSize)
+		xQueuepassword, _ := Encrypt("123456", MySecret)
+		Queuepassword = MyApp.Preferences().StringWithFallback("Queuepasword", xQueuepassword)
+
+		var xCaroot = strings.ReplaceAll("-----BEGIN CERTIFICATE-----\nMIICFDCCAbugAwIBAgIUDkHxHO1DwrlkTzUimG5PoiswB6swCgYIKoZIzj0EAwIw\nZjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkZMMQswCQYDVQQHEwJDVjEMMAoGA1UE\nChMDU0VDMQwwCgYDVQQLEwNuaDExITAfBgNVBAMTGG5hdHMubmV3aG9yaXpvbnMz\nMDAwLm9yZzAgFw0yMzAzMzExNzI5MDBaGA8yMDUzMDMyMzE3MjkwMFowZjELMAkG\nA1UEBhMCVVMxCzAJBgNVBAgTAkZMMQswCQYDVQQHEwJDVjEMMAoGA1UEChMDU0VD\nMQwwCgYDVQQLEwNuaDExITAfBgNVBAMTGG5hdHMubmV3aG9yaXpvbnMzMDAwLm9y\nZzBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABHXwMUfMXiJix3tuzFymcA+3RkeY\nZE7urUzVgaqkv/Oef3jhqhtf1XzK/qVYGxWWmpvADGB252PG1Mp7Z5wmzqyjRTBD\nMA4GA1UdDwEB/wQEAwIBBjASBgNVHRMBAf8ECDAGAQH/AgEBMB0GA1UdDgQWBBQm\nFA5caanuqxGFOf9DtZkVYv5dCzAKBggqhkjOPQQDAgNHADBEAiB3BheNP4XdBZ27\nxVBQ7ztMJqK7wDi1V3LuMy5jmXr7rQIgHCse0oaiAwcl4VwF00aSshlV+T/da0Tx\n1ANkaM+rie4=\n-----END CERTIFICATE-----\n", "\n", "<>")
+		ycaroot, _ := Encrypt(xCaroot, MySecret)
+		Caroot = MyApp.Preferences().StringWithFallback("Caroot", ycaroot)
+
+		var xClientcert = strings.ReplaceAll("-----BEGIN CERTIFICATE-----\nMIIDUzCCAvigAwIBAgIUUyhlJt8mp1XApRbSkdrUS55LGV8wCgYIKoZIzj0EAwIw\nZjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkZMMQswCQYDVQQHEwJDVjEMMAoGA1UE\nChMDU0VDMQwwCgYDVQQLEwNuaDExITAfBgNVBAMTGG5hdHMubmV3aG9yaXpvbnMz\nMDAwLm9yZzAeFw0yMzAzMzExNzI5MDBaFw0yODAzMjkxNzI5MDBaMHIxCzAJBgNV\nBAYTAlVTMRAwDgYDVQQIEwdGbG9yaWRhMRIwEAYDVQQHEwlDcmVzdHZpZXcxGjAY\nBgNVBAoTEU5ldyBIb3Jpem9ucyAzMDAwMSEwHwYDVQQLExhuYXRzLm5ld2hvcml6\nb25zMzAwMC5vcmcwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDFttVH\nQ131JYwazAQMm0XAQvRvTjTjOY3aei1++mmQ+NQ9mrOFk6HlZFoKqsy6+HPXsB9x\nQbWlYvUOuqBgb9xFQZoL8jiKskLLrXoIxUAlIBTlyf76r4SV+ZpxJYoGzXNTedaU\n0EMTyAiUQ6nBbFMXiehN5q8VzxtTESk7QguGdAUYXYsCmYBvQtBXoFYO5CHyhPqu\nOZh7PxRAruYypEWVFBA+29+pwVeaRHzpfd/gKLY4j2paInFn7RidYUTqRH97BjdR\nSZpOJH6fD7bI4L09pnFtII5pAARSX1DntS0nWIWhYYI9use9Hi/B2DRQLcDSy1G4\n0t1z4cdyjXxbFENTAgMBAAGjgawwgakwDgYDVR0PAQH/BAQDAgWgMBMGA1UdJQQM\nMAoGCCsGAQUFBwMCMAwGA1UdEwEB/wQCMAAwHQYDVR0OBBYEFAzgPVB2/sfT7R0U\ne3iXRSvUkfoQMB8GA1UdIwQYMBaAFCYUDlxpqe6rEYU5/0O1mRVi/l0LMDQGA1Ud\nEQQtMCuCGG5hdHMubmV3aG9yaXpvbnMzMDAwLm9yZ4IJMTI3LDAsMCwxhwTAqABn\nMAoGCCqGSM49BAMCA0kAMEYCIQCDlUH2j69mJ4MeKvI8noOmvLHfvP4qMy5nFW2F\nPT5UxgIhAL6pHFyEbANtSkcVJqxTyKE4GTXcHc4DB43Z1F7VxSJj\n-----END CERTIFICATE-----\n", "\n", "<>")
+		yclientcert, _ := Encrypt(xClientcert, MySecret)
+		Clientcert = MyApp.Preferences().StringWithFallback("Clientcert", yclientcert)
+
+		var xClientkey = strings.ReplaceAll("-----BEGIN RSA PRIVATE KEY-----\nMIIEowIBAAKCAQEAxbbVR0Nd9SWMGswEDJtFwEL0b0404zmN2notfvppkPjUPZqz\nhZOh5WRaCqrMuvhz17AfcUG1pWL1DrqgYG/cRUGaC/I4irJCy616CMVAJSAU5cn+\n+q+ElfmacSWKBs1zU3nWlNBDE8gIlEOpwWxTF4noTeavFc8bUxEpO0ILhnQFGF2L\nApmAb0LQV6BWDuQh8oT6rjmYez8UQK7mMqRFlRQQPtvfqcFXmkR86X3f4Ci2OI9q\nWiJxZ+0YnWFE6kR/ewY3UUmaTiR+nw+2yOC9PaZxbSCOaQAEUl9Q57UtJ1iFoWGC\nPbrHvR4vwdg0UC3A0stRuNLdc+HHco18WxRDUwIDAQABAoIBACe0XMZP4Al//c/P\n0qxZbjt69q13jiVnhHYwfPx3+0UywySP8adMi4GOkop73Ftb05+n7diHspvA8KeB\nkP1s2VZLI01s2i/4NnPCpbQnMIeEFs5Cr2LWZpDbrEk2ma5eCd/kotQFssLBM//a\nSrfeMh2TA0TJo7WEft9Cnf4ZeEkKnycplfvwTyv286iFZCYo2dv66BfTej6kkVCo\nAi+ZVCe2zSqRYyr0u4/j/kE3b3eSkCnY2IVcqlP7epuEGVOZyxeFLwM5ljbWL816\npA6WIJgQo2EQ1N7L531neg5WjXQ/UwTQoXP1jvuuVtKtOBFqm1IshEyFk3WpsfpD\nr16OTdECgYEA6FB6NYxYtnWPaIYAOqP7GtMKoJujH8MtZy6J33LkxI7nPkMkn8Mv\nva32tvjU4Bu1FVNp9k5guC+b+8ixXK0URj25IOhDs6K57tck22W9WiTZlmnkCO01\nJOavrelWbvYt5xNWIdnPualoPfGB0iJKXsKY/bpH4eVfhWwpNPI5sMkCgYEA2d9G\nEPuWN6gUjZ+JfdS+0WHK1yGD7thXs7MPUlhGqDzBryh2dkywyo8U8+tMLuDok1RZ\njnT3PYkLQEpzoV0qBkpFFShL6ubaGmDz1UZsozl0YcIg4diZeuPHnIAeXOFrhgYf\n825163LmT3jYHCROFEMLtTYyIQP0EznE+qFT3TsCgYEApgtvbfqkJbWdDL5KR5+R\nCLky7VyQmVEtkIRI8zbxoDPrwCrJcI9X/iDrKBhuPshPA7EdGXkn1D3jJXFqo6zp\nwtK3EXgxe6Ghd766jz4Guvl/s+x3mpHA3GEtzAXtS14VrQW7GHLP8AnPggauHX14\n3oYER8XvPtxtC7YlNbyz01ECgYAe2b7SKM3ck7BVXYHaj4V1oKNYUyaba4b/qxtA\nTb+zkubaJqCfn7xo8lnFMExZVv+X3RnRUj6wN/ef4ur8rnSE739Yv5wAZy/7DD96\ns74uXrRcI2EEmechv59ESeACxuiy0as0jS+lZ1+1YSc41Os5c0T1I/d1NVoaXtPF\nqZJ2gQKBgBp/XavdULBPzC7B8tblySzmL01qJZV7MSSVo2/1vJ7gPM0nQPZdTDog\nTfA5QKSX9vFTGC9CZHSJ+fabYDDd6+3UNYUKINfr+kwu9C2cysbiPaM3H27WR5mW\n5LhStAfwuRRYBDsG2ndjraxcBrrPdtkbS0dpeQUDJxvkMIuLHnhQ\n-----END RSA PRIVATE KEY-----\n", "\n", "<>")
+		yclientkey, _ := Encrypt(xClientkey, MySecret)
+		Clientkey = MyApp.Preferences().StringWithFallback("Clientkey", yclientkey)
+		PasswordMinimumSize = MyApp.Preferences().StringWithFallback("PasswordMinimumSize", "6")
 		PasswordMustContainNumber = MyApp.Preferences().BoolWithFallback("PasswordMustContainNumber", true)
 		PasswordMustContainLetter = MyApp.Preferences().BoolWithFallback("PasswordMustContainLetter", true)
 		PasswordMustContainSpecial = MyApp.Preferences().BoolWithFallback("PasswordMustContainSpecial", true)
+
+		// prepare for operations
 		yServer, _ := Decrypt(Server, MySecret)
 		Server = yServer
 		yQueue, _ := Decrypt(Queue, MySecret)
@@ -151,8 +167,21 @@ func MyJson(action string) {
 		Alias = yAlias
 		yQueuepassword, _ := Decrypt(Queuepassword, MySecret)
 		Queuepassword = yQueuepassword
+		yCaroot, _ := Decrypt(Caroot, MySecret)
+		Caroot = strings.ReplaceAll(yCaroot, "<>", "\n")
+		yClientcert, _ := Decrypt(Clientcert, MySecret)
+		Clientcert = strings.ReplaceAll(yClientcert, "<>", "\n")
+		yClientkey, _ := Decrypt(Clientkey, MySecret)
+		Clientkey = strings.ReplaceAll(yClientkey, "<>", "\n")
 	}
 	if action == "SAVE" {
+		xCaroot, _ := Encrypt(Caroot, MySecret)
+		MyApp.Preferences().SetString("Caroot", xCaroot)
+		xClientcert, _ := Encrypt(Clientcert, MySecret)
+		MyApp.Preferences().SetString("Clientcert", xClientcert)
+		xClientkey, _ := Encrypt(Clientkey, MySecret)
+		MyApp.Preferences().SetString("Clientkey", xClientkey)
+
 		xServer, _ := Encrypt(Server, MySecret)
 		MyApp.Preferences().SetString("Server", xServer)
 		xQueue, _ := Encrypt(Queue, MySecret)
@@ -177,7 +206,8 @@ func Encode(b []byte) string {
 func Decode(s string) []byte {
 	data, err := base64.StdEncoding.DecodeString(s)
 	if err != nil {
-		panic(err)
+		log.Println("DataStore Decode ", s, "  ", err)
+		log.Panic(err)
 	}
 	return data
 }
@@ -247,15 +277,16 @@ func Decrypt(text string, MySecret string) (string, error) {
 func MyHash(action string) bool {
 
 	if action == "CREATE" {
-		//log.Println("MyHash  CREATE ", DataStore("config.hash"))
+
 		err := storage.Delete(DataStore("config.hash"))
 		if err != nil {
 			log.Println("MyHash Error Deleting", DataStore("config.hash"))
 		}
 		wrt, errwrite := storage.Writer(DataStore("config.hash"))
 		_, err2 := wrt.Write([]byte(Passwordhash))
+		log.Println("MyHash create", Passwordhash)
 		if errwrite != nil || err2 != nil {
-			log.Println("MyHash Error Writing", DataStore("config.hash"))
+
 			return true
 		}
 
@@ -263,7 +294,6 @@ func MyHash(action string) bool {
 	if action == "LOAD" {
 		ph, errf := os.ReadFile(DataStore("config.hash").Path())
 		Passwordhash = string(ph)
-
 		if errf != nil {
 			log.Println("MyHash LOAD Hash Error file", errf, " ", Passwordhash)
 			return true
@@ -342,11 +372,11 @@ func FormatMessage(m string) []byte {
 
 	name, err := os.Hostname()
 	if err != nil {
-		EncMessage.MShostname = "No Host Name"
+		EncMessage.MShostname = "\nNo Host Name"
 		//strings.Replace(EncMessage, "#HOSTNAME", "No Host Name", -1)
 
 	} else {
-		EncMessage.MShostname = name
+		EncMessage.MShostname = "\nHost - " + name
 		//strings.Replace(EncMessage, "#HOSTNAME", name, -1)
 	}
 	ifas, err := net.Interfaces()
@@ -359,22 +389,23 @@ func FormatMessage(m string) []byte {
 				as = append(as, a)
 			}
 		}
+		EncMessage.MShostname += "\nMac ids"
 		for i, s := range as {
-			EncMessage.MShostname += " mac: " + strconv.Itoa(i) + " - " + s + ","
+			EncMessage.MShostname += "\n- " + strconv.Itoa(i) + " : " + s
 		}
 		addrs, _ := net.InterfaceAddrs()
-
+		EncMessage.MShostname += "\nAddress"
 		for _, addr := range addrs {
-			EncMessage.MShostname += "\n addrs: " + addr.String() + ","
+			EncMessage.MShostname += "\n- " + addr.String()
 		}
 
 	}
 
 	EncMessage.MSalias = Alias
 
-	EncMessage.MSnodeuuid = NodeUUID
-
-	EncMessage.MSiduuid = "Node: " + uuid.New().String()
+	EncMessage.MSnodeuuid = "\nNode Id - " + NodeUUID[0:8]
+	iduuid := uuid.New().String()
+	EncMessage.MSiduuid = "\nMessage Id - " + iduuid[0:8]
 
 	EncMessage.MSmessage = m
 	//EncMessage += m
@@ -450,6 +481,51 @@ func editEntry(action string, value string) bool {
 			return true
 		}
 		return false
+	}
+
+	if action == "PASSWORD" {
+		var iserrors = false
+		vlen, _ := strconv.Atoi(PasswordMinimumSize)
+		if len(value) <= vlen {
+			iserrors = true
+			log.Println("editentry length ", iserrors)
+		}
+		log.Println("password.go letter b4 ", iserrors)
+		if PasswordMustContainLetter && !iserrors {
+			iserrors = true
+			for _, r := range value {
+				if (r < 'a' || r > 'z') && (r < 'A' || r > 'Z') {
+					iserrors = false
+					log.Println("editentry letter ", iserrors)
+					break
+				}
+			}
+		}
+		log.Println("password.go number b4 ", iserrors)
+		if PasswordMustContainNumber && !iserrors {
+			iserrors = true
+			for _, r := range value {
+				if unicode.IsNumber(r) {
+					iserrors = false
+					log.Println("password.go number", iserrors)
+					break
+				}
+			}
+		}
+		log.Println("password.go special b4 ", iserrors)
+		if PasswordMustContainSpecial && !iserrors {
+			iserrors = true
+			var schars = []string{"|", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "}", "]", "[", "|", ":", ";", ",", ".", "#", "'", "\"", "\\", "%", "?", "\n", "<", "Ø", "ð", ">", "ï", "û"}
+			for _, sc := range schars {
+				if strings.Contains(value, sc) {
+					iserrors = false
+					log.Println("password.go special ", iserrors)
+					break
+				}
+			}
+		}
+		log.Println("password.go special a4 ", iserrors)
+		return iserrors
 	}
 	if action == "CERTIFICATE" {
 		valid := strings.Contains(value, "-----BEGIN CERTIFICATE-----")
