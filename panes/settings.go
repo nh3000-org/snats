5343e09b6b40aecd0eac1fc57fea430508f0ad2a
nh3000-org <118835575+nh3000-org@users.noreply.github.com>
2023-06-25 16:12:23 -0500

    BETA.2

--- panes/settings.go
+++ panes/settings.go
@@ -8,10 +8,20 @@ import (
 
 func settingsScreen(_ fyne.Window) fyne.CanvasObject {
 	MyJson("LOAD")
+	lalabel := widget.NewLabel("Prefered Language")
+	la := widget.NewRadioGroup([]string{"eng", "esp"}, func(string) {})
+	la.Horizontal = true
+	la.SetSelected(PreferedLanguage)
+
 	pllabel := widget.NewLabel("Password Length")
 	pl := widget.NewRadioGroup([]string{"6", "8", "12"}, func(string) {})
 	pl.Horizontal = true
 	pl.SetSelected(PasswordMinimumSize)
+
+	malabel := widget.NewLabel("Message Max Age In Hours")
+	ma := widget.NewRadioGroup([]string{"8h", "16h", "24h", "48h", "161h", "8372h"}, func(string) {})
+	ma.Horizontal = true
+	ma.SetSelected(Msgmaxage)
 
 	mcletterlabel := widget.NewLabel("Password Must Contain Letter")
 	mcletter := widget.NewRadioGroup([]string{"True", "False"}, func(string) {})
@@ -29,6 +39,8 @@ func settingsScreen(_ fyne.Window) fyne.CanvasObject {
 	mcspecial.SetSelected(PasswordMustContainSpecial)
 
 	ssbutton := widget.NewButton("Save Settings", func() {
+		PreferedLanguage = la.Selected
+		Msgmaxage = ma.Selected
 		PasswordMustContainNumber = mcnumber.Selected
 		PasswordMinimumSize = pl.Selected
 		PasswordMustContainLetter = mcletter.Selected
@@ -40,7 +52,10 @@ func settingsScreen(_ fyne.Window) fyne.CanvasObject {
 
 	return container.NewCenter(container.NewVBox(
 		widget.NewLabelWithStyle("New Horizons 3000 Secure Communications ", fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
-
+		lalabel,
+		la,
+		malabel,
+		ma,
 		pllabel,
 		pl,
 		mcletterlabel,
