f794d64670286b208670ab975dc6556379a0b972
nh3000-org <118835575+nh3000-org@users.noreply.github.com>
2023-06-29 16:05:01 -0500

    BETA.2.1

--- panes/password.go
+++ panes/password.go
@@ -13,51 +13,47 @@ import (
 func passwordScreen(_ fyne.Window) fyne.CanvasObject {
 
 	password := widget.NewPasswordEntry()
-	password.SetPlaceHolder("Enter Password For Encryption")
-	password.SetText(Password)
+	password.SetPlaceHolder(GetLangs("ps-password"))
 
 	passwordc1 := widget.NewPasswordEntry()
-	passwordc1.SetPlaceHolder("Enter Change Password")
-	//passwordc1.SetText(Password)
+	passwordc1.SetPlaceHolder(GetLangs("ps-passwordc1"))
 	passwordc1.Disable()
 
 	passwordc2 := widget.NewPasswordEntry()
-	passwordc2.SetPlaceHolder("Enter Change Password Again")
-	//passwordc1.SetText(Password)
+	passwordc2.SetPlaceHolder(GetLangs("ps-passwordc2"))
 	passwordc2.Disable()
 	errors := widget.NewLabel("...")
 	// try the password
-	tpbutton := widget.NewButton("Try Password", func() {
+	tpbutton := widget.NewButton(GetLangs("ps-trypassword"), func() {
 		var iserrors = false
 		Password = password.Text
 		pwh, err := bcrypt.GenerateFromPassword([]byte(Password), bcrypt.DefaultCost)
 		Passwordhash = string(pwh)
 		if err != nil {
 			iserrors = true
-			log.Println("Error Creating Password Hash ", err)
-			errors.SetText(err.Error())
+			errors.SetText(GetLangs("ps-err1"))
 		}
-		_, confighasherr := os.Stat("config.hash")
+		_, confighasherr := os.Stat(DataStore("config.hash").Path())
 		if confighasherr != nil {
 
 			if MyHash("CREATE") {
-				errors.SetText("Error Creating Password Hash")
+				errors.SetText(GetLangs("ps-err2"))
 			}
 		}
 
 		Password = password.Text
 		if MyHash("LOAD") {
-			errors.SetText("Error Reading Password Hash")
+			errors.SetText(GetLangs("ps-err3"))
 		}
 		// Comparing the password with the hash
 		if err := bcrypt.CompareHashAndPassword([]byte(Passwordhash), []byte(Password)); err != nil {
 
 			iserrors = true
-			errors.SetText(err.Error())
+			errors.SetText(GetLangs("ps-err4"))
 		}
 		if !iserrors {
 			MyJson("LOAD")
-			errors.SetText("Password Accepted ")
+			errors.SetText(GetLangs("ps-err5"))
 
 			password.Disable()
 			passwordc1.Enable()
@@ -66,49 +62,45 @@ func passwordScreen(_ fyne.Window) fyne.CanvasObject {
 		}
 	})
 
-	cpbutton := widget.NewButton("Change Password", func() {
+	cpbutton := widget.NewButton(GetLangs("ps-chgpassword"), func() {
 		var iserrors = false
 
 		if editEntry("STRING", passwordc1.Text) == true {
 			iserrors = true
-			errors.SetText("Error Pasword 1 Invalid")
+			errors.SetText(GetLangs("ps-err6"))
 		}
 
-		log.Println("password.go iserrors b4 pass ", iserrors)
 		if editEntry("PASSWORD", passwordc1.Text) {
 			iserrors = true
-			errors.SetText("Error Pasword Does Not Meet Requirements")
+			errors.SetText(GetLangs("ps-err7"))
 		}
 		if passwordc1.Text != passwordc2.Text {
 			iserrors = true
-			errors.SetText("Error Pasword 1 Does Not Match Password 2")
+			errors.SetText(GetLangs("ps-err8"))
 		}
-		log.Println("password.go iserrors", iserrors)
 		if !iserrors {
 			pwh, err := bcrypt.GenerateFromPassword([]byte(passwordc1.Text), bcrypt.DefaultCost)
 			Passwordhash = string(pwh)
-			log.Println("password hash", Passwordhash)
+
 			if err != nil {
 				log.Fatal(err)
 			}
-			log.Println("change save ", Passwordhash, " ", DataStore("config.hash"))
+
 			if MyHash("SAVE") {
-				errors.SetText("Error Saving Password Hash")
-				log.Println("password.go Error Saving Password Hash ")
+				errors.SetText(GetLangs("ps-err9"))
+
 			}
 
 		}
 		Password = passwordc1.Text
 		if MyHash("LOAD") {
-			errors.SetText("Error Reading Password Hash")
-			log.Println("password.go Error Reading Password Hash ")
+			errors.SetText(GetLangs("ps-err10"))
 			iserrors = true
 		}
 		// Comparing the password with the hash
 		err := bcrypt.CompareHashAndPassword([]byte(Passwordhash), []byte(passwordc1.Text))
 		if err != nil {
-			log.Println("Error Invalid Password ", err)
-			errors.SetText("password.go Error Invalid Password ")
+			errors.SetText(GetLangs("ps-err11"))
 			iserrors = true
 		}
 		if !iserrors {
@@ -119,11 +111,13 @@ func passwordScreen(_ fyne.Window) fyne.CanvasObject {
 	})
 
 	return container.NewCenter(container.NewVBox(
-		widget.NewLabelWithStyle("New Horizons 3000 Secure Communications", fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
-		widget.NewLabelWithStyle("Enter Password To Reset", fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
-		widget.NewLabelWithStyle("For Local Security", fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
+		widget.NewLabelWithStyle(GetLangs("ps-title1"), fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
+		widget.NewLabelWithStyle("config.json", fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
+		widget.NewLabelWithStyle(GetLangs("ps-title2"), fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
+
 		password,
 		tpbutton,
+		widget.NewLabelWithStyle(GetLangs("ps-title3"), fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
 		passwordc1,
 		passwordc2,
 		cpbutton,
