a720677dc8e23367182a665a14b30d6f495ccb75
nh3000-org <118835575+nh3000-org@users.noreply.github.com>
2023-08-03 19:31:49 -0500

    BETA.2

--- log.go
+++ log.go
@@ -30,11 +30,15 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/nats-io/nats.go"
+	"github.com/nh3000-org/snats/panes"
 	//"os"
 )
 
 var idcount int
 var MyLogLang string
+var LogCaroot = "-----BEGIN CERTIFICATE-----\nMIICFDCCAbugAwIBAgIUDkHxHO1DwrlkTzUimG5PoiswB6swCgYIKoZIzj0EAwIw\nZjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkZMMQswCQYDVQQHEwJDVjEMMAoGA1UE\nChMDU0VDMQwwCgYDVQQLEwNuaDExITAfBgNVBAMTGG5hdHMubmV3aG9yaXpvbnMz\nMDAwLm9yZzAgFw0yMzAzMzExNzI5MDBaGA8yMDUzMDMyMzE3MjkwMFowZjELMAkG\nA1UEBhMCVVMxCzAJBgNVBAgTAkZMMQswCQYDVQQHEwJDVjEMMAoGA1UEChMDU0VD\nMQwwCgYDVQQLEwNuaDExITAfBgNVBAMTGG5hdHMubmV3aG9yaXpvbnMzMDAwLm9y\nZzBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABHXwMUfMXiJix3tuzFymcA+3RkeY\nZE7urUzVgaqkv/Oef3jhqhtf1XzK/qVYGxWWmpvADGB252PG1Mp7Z5wmzqyjRTBD\nMA4GA1UdDwEB/wQEAwIBBjASBgNVHRMBAf8ECDAGAQH/AgEBMB0GA1UdDgQWBBQm\nFA5caanuqxGFOf9DtZkVYv5dCzAKBggqhkjOPQQDAgNHADBEAiB3BheNP4XdBZ27\nxVBQ7ztMJqK7wDi1V3LuMy5jmXr7rQIgHCse0oaiAwcl4VwF00aSshlV+T/da0Tx\n1ANkaM+rie4=\n-----END CERTIFICATE-----\n"
+var LogClientcert = "-----BEGIN CERTIFICATE-----\nMIIDUzCCAvigAwIBAgIUUyhlJt8mp1XApRbSkdrUS55LGV8wCgYIKoZIzj0EAwIw\nZjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkZMMQswCQYDVQQHEwJDVjEMMAoGA1UE\nChMDU0VDMQwwCgYDVQQLEwNuaDExITAfBgNVBAMTGG5hdHMubmV3aG9yaXpvbnMz\nMDAwLm9yZzAeFw0yMzAzMzExNzI5MDBaFw0yODAzMjkxNzI5MDBaMHIxCzAJBgNV\nBAYTAlVTMRAwDgYDVQQIEwdGbG9yaWRhMRIwEAYDVQQHEwlDcmVzdHZpZXcxGjAY\nBgNVBAoTEU5ldyBIb3Jpem9ucyAzMDAwMSEwHwYDVQQLExhuYXRzLm5ld2hvcml6\nb25zMzAwMC5vcmcwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDFttVH\nQ131JYwazAQMm0XAQvRvTjTjOY3aei1++mmQ+NQ9mrOFk6HlZFoKqsy6+HPXsB9x\nQbWlYvUOuqBgb9xFQZoL8jiKskLLrXoIxUAlIBTlyf76r4SV+ZpxJYoGzXNTedaU\n0EMTyAiUQ6nBbFMXiehN5q8VzxtTESk7QguGdAUYXYsCmYBvQtBXoFYO5CHyhPqu\nOZh7PxRAruYypEWVFBA+29+pwVeaRHzpfd/gKLY4j2paInFn7RidYUTqRH97BjdR\nSZpOJH6fD7bI4L09pnFtII5pAARSX1DntS0nWIWhYYI9use9Hi/B2DRQLcDSy1G4\n0t1z4cdyjXxbFENTAgMBAAGjgawwgakwDgYDVR0PAQH/BAQDAgWgMBMGA1UdJQQM\nMAoGCCsGAQUFBwMCMAwGA1UdEwEB/wQCMAAwHQYDVR0OBBYEFAzgPVB2/sfT7R0U\ne3iXRSvUkfoQMB8GA1UdIwQYMBaAFCYUDlxpqe6rEYU5/0O1mRVi/l0LMDQGA1Ud\nEQQtMCuCGG5hdHMubmV3aG9yaXpvbnMzMDAwLm9yZ4IJMTI3LDAsMCwxhwTAqABn\nMAoGCCqGSM49BAMCA0kAMEYCIQCDlUH2j69mJ4MeKvI8noOmvLHfvP4qMy5nFW2F\nPT5UxgIhAL6pHFyEbANtSkcVJqxTyKE4GTXcHc4DB43Z1F7VxSJj\n-----END CERTIFICATE-----\n"
+var LogClientkey = "-----BEGIN RSA PRIVATE KEY-----\nMIIEowIBAAKCAQEAxbbVR0Nd9SWMGswEDJtFwEL0b0404zmN2notfvppkPjUPZqz\nhZOh5WRaCqrMuvhz17AfcUG1pWL1DrqgYG/cRUGaC/I4irJCy616CMVAJSAU5cn+\n+q+ElfmacSWKBs1zU3nWlNBDE8gIlEOpwWxTF4noTeavFc8bUxEpO0ILhnQFGF2L\nApmAb0LQV6BWDuQh8oT6rjmYez8UQK7mMqRFlRQQPtvfqcFXmkR86X3f4Ci2OI9q\nWiJxZ+0YnWFE6kR/ewY3UUmaTiR+nw+2yOC9PaZxbSCOaQAEUl9Q57UtJ1iFoWGC\nPbrHvR4vwdg0UC3A0stRuNLdc+HHco18WxRDUwIDAQABAoIBACe0XMZP4Al//c/P\n0qxZbjt69q13jiVnhHYwfPx3+0UywySP8adMi4GOkop73Ftb05+n7diHspvA8KeB\nkP1s2VZLI01s2i/4NnPCpbQnMIeEFs5Cr2LWZpDbrEk2ma5eCd/kotQFssLBM//a\nSrfeMh2TA0TJo7WEft9Cnf4ZeEkKnycplfvwTyv286iFZCYo2dv66BfTej6kkVCo\nAi+ZVCe2zSqRYyr0u4/j/kE3b3eSkCnY2IVcqlP7epuEGVOZyxeFLwM5ljbWL816\npA6WIJgQo2EQ1N7L531neg5WjXQ/UwTQoXP1jvuuVtKtOBFqm1IshEyFk3WpsfpD\nr16OTdECgYEA6FB6NYxYtnWPaIYAOqP7GtMKoJujH8MtZy6J33LkxI7nPkMkn8Mv\nva32tvjU4Bu1FVNp9k5guC+b+8ixXK0URj25IOhDs6K57tck22W9WiTZlmnkCO01\nJOavrelWbvYt5xNWIdnPualoPfGB0iJKXsKY/bpH4eVfhWwpNPI5sMkCgYEA2d9G\nEPuWN6gUjZ+JfdS+0WHK1yGD7thXs7MPUlhGqDzBryh2dkywyo8U8+tMLuDok1RZ\njnT3PYkLQEpzoV0qBkpFFShL6ubaGmDz1UZsozl0YcIg4diZeuPHnIAeXOFrhgYf\n825163LmT3jYHCROFEMLtTYyIQP0EznE+qFT3TsCgYEApgtvbfqkJbWdDL5KR5+R\nCLky7VyQmVEtkIRI8zbxoDPrwCrJcI9X/iDrKBhuPshPA7EdGXkn1D3jJXFqo6zp\nwtK3EXgxe6Ghd766jz4Guvl/s+x3mpHA3GEtzAXtS14VrQW7GHLP8AnPggauHX14\n3oYER8XvPtxtC7YlNbyz01ECgYAe2b7SKM3ck7BVXYHaj4V1oKNYUyaba4b/qxtA\nTb+zkubaJqCfn7xo8lnFMExZVv+X3RnRUj6wN/ef4ur8rnSE739Yv5wAZy/7DD96\ns74uXrRcI2EEmechv59ESeACxuiy0as0jS+lZ1+1YSc41Os5c0T1I/d1NVoaXtPF\nqZJ2gQKBgBp/XavdULBPzC7B8tblySzmL01qJZV7MSSVo2/1vJ7gPM0nQPZdTDog\nTfA5QKSX9vFTGC9CZHSJ+fabYDDd6+3UNYUKINfr+kwu9C2cysbiPaM3H27WR5mW\n5LhStAfwuRRYBDsG2ndjraxcBrrPdtkbS0dpeQUDJxvkMIuLHnhQ\n-----END RSA PRIVATE KEY-----\n"
 
 type MessageStore struct {
 	MSiduuid   string
@@ -46,50 +50,14 @@ type MessageStore struct {
 	MSdate     string
 }
 
-var MyLangs = map[string]string{
-	"eng-mn-alias": "Intrusion Detection",
-	"esp-mn-alias": "Detección de Intrusos",
-	"eng-mn-lc":    "Log Connection",
-	"esp-mn-lc":    "Conexión de Registro",
-	"eng-fm-nhn":   "No Host Name",
-	"esp-fm-nhn":   "Sin Nombre de Host",
-	"eng-fm-hn":    "Host Name",
-	"esp-fm-hn":    "Nombre de Host",
-	"eng-fm-mi":    "Mac Ids",
-	"esp-fm-mi":    "Identificadores de Mac",
-	"eng-fm-ad":    "Address",
-	"esp-fm-ad":    "Direccion",
-	"eng-fm-ni":    "Node Id",
-	"esp-fm-ni":    "Identificación del Nodo",
-	"eng-fm-msg":   "Message Id",
-	"esp-fm-msg":   "Identificación del mensaje",
-	"eng-fm-on":    "On",
-	"esp-fm-on":    "En",
-	"eng-fm-fm":    "Format Message",
-	"esp-fm-fm":    "Dar Formato al Mensaje",
-	"eng-fm-con":   "Connection ",
-	"esp-fm-con":   "Conexión ",
-	"eng-fm-js":    "Jet Stream ",
-	"esp-fm-js":    "Corriente en Chorro ",
-}
-
-func GetLangs(mylang, mystring string) string {
-
-	value, err := MyLangs[mylang+"-"+mystring]
-	//log.Println("GetLangs ", PreferedLanguage+"-"+mystring)
-	if err == false {
-		return "err"
-	}
-	return value
-}
 func FormatMessage(m string) []byte {
 	EncMessage := MessageStore{}
 
 	name, err := os.Hostname()
 	if err != nil {
-		EncMessage.MShostname = "\n" + GetLangs(MyLogLang, "fm-nhn")
+		EncMessage.MShostname = "\n" + panes.GetLangs("fm-nhn")
 	} else {
-		EncMessage.MShostname = "\n" + GetLangs(MyLogLang, "fm-hn") + " - " + name
+		EncMessage.MShostname = "\n" + panes.GetLangs("fm-hn") + " - " + name
 	}
 	ifas, err := net.Interfaces()
 	if err == nil {
@@ -101,56 +69,79 @@ func FormatMessage(m string) []byte {
 				as = append(as, a)
 			}
 		}
-		EncMessage.MShostname += "\n" + GetLangs(MyLogLang, "fm-mi")
+		EncMessage.MShostname += "\n" + panes.GetLangs("fm-mi")
 		for i, s := range as {
 			EncMessage.MShostname += "\n- " + strconv.Itoa(i) + " : " + s
 		}
 		addrs, _ := net.InterfaceAddrs()
-		EncMessage.MShostname += "\n" + GetLangs(MyLogLang, "fm-ad")
+		EncMessage.MShostname += "\n" + panes.GetLangs("fm-ad")
 		for _, addr := range addrs {
 			EncMessage.MShostname += "\n- " + addr.String()
 		}
 
 	}
 
-	EncMessage.MSalias = GetLangs(MyLogLang, "mn-alias")
+	EncMessage.MSalias = panes.GetLangs("mn-alias")
 	idcount++
-	EncMessage.MSnodeuuid = "\n" + GetLangs(MyLogLang, "mn-ni") + " - " + strconv.Itoa(idcount)
+	EncMessage.MSnodeuuid = "\n" + panes.GetLangs("mn-ni") + " - " + strconv.Itoa(idcount)
 	iduuid := uuid.New().String()
-	EncMessage.MSiduuid = "\n" + GetLangs(MyLogLang, "mn-msg") + " - " + iduuid[0:8]
-	EncMessage.MSdate = "\n" + GetLangs(MyLogLang, "mn-on") + " -" + time.Now().Format(time.UnixDate)
+	EncMessage.MSiduuid = "\n" + panes.GetLangs("mn-msg") + " - " + iduuid[0:8]
+	EncMessage.MSdate = "\n" + panes.GetLangs("mn-on") + " -" + time.Now().Format(time.UnixDate)
 	//EncMessage.MSdate = "\nOn -"
 	EncMessage.MSmessage = m
 	//EncMessage += m
 	jsonmsg, jsonerr := json.Marshal(EncMessage)
 	if jsonerr != nil {
-		log.Println(GetLangs(MyLogLang, "mn-fm"), jsonerr)
+		log.Println(panes.GetLangs("mn-fm"), jsonerr)
 	}
-
-	return []byte(jsonmsg)
+	ejson, _ := panes.Encrypt(string(jsonmsg), "123456789012345678901234")
+	return []byte(ejson)
 
 }
 func main() {
 	//[67367] 2023/07/27 16:55:21.707056 [ERR] 87.236.176.182:54369 - cid:115 - TLS handshake error: EOF
+
 	logLang := flag.String("loglang", "eng", "NATS Language to Use eng esp")
 	logPattern := flag.String("logpattern", "[ERR]", "Log Pattern to Identify")
 	CA := flag.String("ca", "./ca.pem", "Path to TLS CA Certificate Authority")
 	ClientCert := flag.String("clientcert", "./clientcert.pem", "Path to TLS Client Cert")
 	ClientKey := flag.String("clientkey", "./clientkey.pem", "Path to TLS Client Key")
+	ServerIP := flag.String("serverip", "nats://127.0.0.1:4222", "Server IP or DNS Name")
 	flag.Parse()
 	fmt.Println("Usage:")
 	fmt.Println("pipe | output of nats-server debug logging to this")
-	fmt.Println("anything with [ERR] will be logged to the nats server")
+	fmt.Println("anything with LOGPATTERN will be logged to the nats server")
 	fmt.Println("plain text")
 	fmt.Println("")
 	fmt.Println("Run Options:")
 	fmt.Println("-loglang: ", *logLang)
 	MyLogLang = *logLang
+	fmt.Println("serverip", *ServerIP)
 	fmt.Println("-logpattern: ", *logPattern)
 	fmt.Println("-ca: ", *CA)
 	fmt.Println("-clientkey: ", *ClientCert)
-	fmt.Println("-clientkey: ", *ClientKey)
-
+	fmt.Println("-clientcert", *ClientCert)
+	panes.PreferedLanguage = MyLogLang
+	_, err := os.Stat(*CA)
+	if err == nil {
+		// load certs into memory
+		fClientcert, err := os.ReadFile(*ClientCert)
+		if err != nil {
+			log.Println("Failed to read clientCert: %v", err)
+		}
+		LogClientcert = string(fClientcert)
+		fClientKey, err := os.ReadFile(*ClientKey)
+		if err != nil {
+			log.Println("Failed to read clientKey: %v", err)
+		}
+		LogClientkey = string(fClientKey)
+		fCaroot, err := os.ReadFile(*CA)
+		if err != nil {
+			log.Println("Failed to read ca: %v", err)
+		}
+		LogCaroot = string(fCaroot)
+		//log.Println("file ca:", LogCaroot)
+	}
 	r := bufio.NewReader(os.Stdin)
 	buf := make([]byte, 0, 4*1024)
 	for {
@@ -163,27 +154,30 @@ func main() {
 			if err == io.EOF {
 				time.Sleep(time.Minute)
 			}
-
 		}
 
 		if int64(len(buf)) != 0 {
-
+			//log.Println("Bufs:", string(buf))
 			if strings.Contains(string(buf), *logPattern) {
-				//log.Println("Bufs:", string(buf))
-				nc, err := nats.Connect("nats://127.0.0.1:4222", nats.RootCAs(*CA), nats.ClientCert(*ClientCert, *ClientKey))
+				log.Println("Bufs------------:", string(buf))
+				//nc, err := nats.Connect(*ServerIP, nats.RootCAs(*CA), nats.ClientCert(*ClientCert, *ClientKey))
+				nc, err := nats.Connect(*ServerIP, nats.RootCAsMem([]byte(LogCaroot)), nats.ClientCertMem([]byte(LogClientcert), []byte(LogClientkey)))
 				if err != nil {
-					log.Println(GetLangs(MyLogLang, "mn-con"), err.Error())
+					log.Println(panes.GetLangs("mn-con"), err.Error())
 				}
-				js, err := nc.JetStream()
-				if err != nil {
-					log.Println(GetLangs(MyLogLang, "mn-js"), err)
+				js, errjs := nc.JetStream()
+				if errjs != nil {
+					log.Println(panes.GetLangs("mn-js"), errjs.Error())
 				}
-				js.Publish("MESSAGES.log", []byte(FormatMessage(string(buf))))
+				_, jserr := js.Publish("messages.log", []byte(FormatMessage(string(buf))))
+				if jserr != nil {
+					log.Println(panes.GetLangs("mn-js"), jserr)
+				}
 			}
 		}
 
 		if err != nil && err != io.EOF {
-			log.Println(err)
+			log.Println("log.go ", err)
 		}
 	}
 
